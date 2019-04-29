5ce453baf1a0c94bb9e1ffbbac252748d9d7f5bb
unknown <rafaelscg@gmail.com>
2019-04-29 12:29:11 -0300

    first commit

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,18 @@
+const { User } = require('../models')
+
+class UserController {
+  create (req, res) {
+    res.render('auth/signup')
+  }
+
+  async store (req, res) {
+    console.log(req.body)
+    req.body.avatar = 'teste.jpg'
+
+    await User.create(req.body)
+
+    return res.redirect('/')
+  }
+}
+
+module.exports = new UserController()
